367cd1dc6c74df4c2007bf0c54cdea74a3acf540
Eduardo <ealvarez@carminaticonsulting.com>
2023-10-26 21:38:59 -0500

    installing vite and new components for react routing

--- cyclopedia/src/main.jsx
+++ cyclopedia/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Header from "./components/Header.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div className="bg-slate-500">
+      <Header />
+      <App />
+    </div>
+  </React.StrictMode>
+);
